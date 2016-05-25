d23b8bb6533cc87b88d098d446fa58c2d383b5da
Ryan Kayu Ching <ryanching@hotmail.com>
2016-05-25 03:36:15 -0700

    Added styling for the index page

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,5 +2,5 @@ class Recipe < ActiveRecord::Base
 
   has_attached_file :image, styles: { medium: "400x400#" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
-
+  validates :image, presence: true
 end
