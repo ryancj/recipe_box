7667b78db2f2eccdfe3890e28e5f7722a526349c
Ryan Kayu Ching <ryanching@hotmail.com>
2016-05-25 03:23:48 -0700

    Implemented file attachment

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,6 @@
 class Recipe < ActiveRecord::Base
-  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" 
+
+  has_attached_file :image, styles: { medium: "400x400#" }
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+
 end
